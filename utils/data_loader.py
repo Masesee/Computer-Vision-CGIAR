95d9634e306df2858f59726f3cec7355cb089096
Enoch Masese <108927143+Masesee@users.noreply.github.com>
2025-03-07 07:16:01 +0300

    Update data_loader.py | Corrected the dataset path

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,8 +1,15 @@
 import tensorflow as tf
+import os
+
+# Set dataset path
+DATASET_PATH = '/kaggle/input/cgiar-root-volume-estimation-challenge'
 
 def load_data():
+    train_dir = os.path.join(DATASET_PATH, "train") # The images are contained in subfolders within this main folder
+    val_dir = os.path.join(DATASET_PATH, "test") # The images are contained in subfolders within this main folder
+    
     train_data = tf.keras.preprocessing.image_dataset_from_directory(
-        'datasets/train', image_size=(224, 224), batch_size=32)
+        train_dir, image_size=(224, 224), batch_size=32)
     val_data = tf.keras.preprocessing.image_dataset_from_directory(
-        'datasets/val', image_size=(224, 224), batch_size=32)
+        val_dir, image_size=(224, 224), batch_size=32)
     return train_data, val_data
