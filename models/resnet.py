c51ec7a0ce6658565e13df431f9524c9461b955e
Enoch Masese <108927143+Masesee@users.noreply.github.com>
2025-03-08 13:12:59 +0300

    Update resnet.py to support regression task

--- models/resnet.py
+++ models/resnet.py
@@ -2,10 +2,12 @@ from tensorflow.keras.applications import ResNet50
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import GlobalAveragePooling2D, Dense
 
-def build_resnet(input_shape, num_classes):
+def build_resnet(input_shape, num_classes=1, regression=False):
     base_model = ResNet50(weights='imagenet', include_top=False, input_shape=input_shape)
     x = GlobalAveragePooling2D()(base_model.output)
     x = Dense(512, activation='relu')(x)
-    output = Dense(num_classes, activation='softmax')(x)
+    # Regression: Output single continuous value instead of classification
+    activation = "linear" if regression else "softmax"
+    output = Dense(num_classes, activation=activation)(x)
     model = Model(inputs=base_model.input, outputs=output)
     return model
