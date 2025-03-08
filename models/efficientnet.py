da15d7ee30478dbe03e9910dc79de33b7d8ef2b6
Enoch Masese <108927143+Masesee@users.noreply.github.com>
2025-03-08 14:49:39 +0300

    Adapted for root volume regression

--- models/efficientnet.py
+++ models/efficientnet.py
@@ -2,7 +2,7 @@ from tensorflow.keras.applications import EfficientNetB3
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import GlobalAveragePooling2D, Dense
 
-def build_efficientnet(input_shape, regression=True):
+def build_efficientnet(input_shape, num_classes=1, regression=True):
     base_model = EfficientNetB3(weights='imagenet', include_top=False, input_shape=input_shape)
     x = GlobalAveragePooling2D()(base_model.output)
     x = Dense(512, activation='relu')(x)
