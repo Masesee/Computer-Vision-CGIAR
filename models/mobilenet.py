5a7e3dec3ccf9313389e17047a7168bfbb287fe0
Enoch Masese <108927143+Masesee@users.noreply.github.com>
2025-03-08 14:50:08 +0300

    Adapted for root volume regression

--- models/mobilenet.py
+++ models/mobilenet.py
@@ -2,7 +2,7 @@ from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import GlobalAveragePooling2D, Dense
 
-def build_mobilenet(input_shape, regression=True):
+def build_mobilenet(input_shape, num_classes=1, regression=True):
     base_model = MobileNetV2(weights='imagenet', include_top=False, input_shape=input_shape)
     x = GlobalAveragePooling2D()(base_model.output)
     x = Dense(512, activation='relu')(x)
